54b84007c1fdf2c60590624477c3d5c1540bf90a
Rico Chao <rchao@sfu.ca>
2019-12-29 16:41:56 -0800

    Pushing new website to github

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var options = {
+  strings: [
+    "a Computer Engineering Student.",
+    "an Aspiring Software Engineer."
+  ],
+  backSpeed: 30,
+  typeSpeed: 30,
+  startDelay: 500,
+  backDelay: 700,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|'
+};
+
+
+var typed = new Typed("#typed", options);
+
